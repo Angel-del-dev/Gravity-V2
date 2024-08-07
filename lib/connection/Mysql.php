ab32532fe15cdfc363d9fbd8cf52e4d59308a16c
AngelNovo <60150253+Angel-del-dev@users.noreply.github.com>
2024-08-07 22:39:42 +0200

    fixed mysql connection fixed parameters

--- lib/connection/Mysql.php
+++ lib/connection/Mysql.php
@@ -46,7 +46,7 @@ class Mysql {
     public function __construct() {
 
         $db_info = Utils::INI()->mysql_database;
-        $qString = sprintf('mysql:host=localhost;dbname=wp_clone', $db_info->host, $db_info->database);
+        $qString = sprintf('mysql:host=%s;dbname=%s', $db_info->host, $db_info->database);
         $this->connection = new pdo($qString, $db_info->user, $db_info->password);
     }
 
